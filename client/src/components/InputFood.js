f3c4ce6d726cc636423902a31d0ec993b3b5fd97
iahudson <iah224@lehigh.edu>
2022-06-13 15:11:36 -0400

    testing

--- client/src/components/InputFood.js
+++ client/src/components/InputFood.js
@@ -1,23 +1,24 @@
 import React, { Fragment, useState } from "react";
+import "./InputFood.css";
 
 const InputFood = () => {
   const [inputs, setInputs] = useState({
     food: "",
     unit: "",
-    carbon: ""
+    carbon: "",
   });
-const {food,unit,carbon} =inputs;
-const onChange = e =>
-setInputs({ ...inputs, [e.target.name]: e.target.value });
+  const { food, unit, carbon } = inputs;
+  const onChange = (e) =>
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
 
-  const onSubmitForm = async e => {
+  const onSubmitForm = async (e) => {
     e.preventDefault();
     try {
       const body = { food, unit, carbon };
       const response = await fetch("/food", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
+        body: JSON.stringify(body),
       });
       //const parseRespond = await response.json();
       //console.log(parseRespond)
@@ -30,22 +31,22 @@ setInputs({ ...inputs, [e.target.name]: e.target.value });
   return (
     <Fragment>
       <h1 className="text-center mt-5">Food</h1>
-      
+
       <form className="d-flex mt-5" onSubmit={onSubmitForm}>
-      <input
+        <input
           type="text"
           name="food"
           value={food}
-          placeholder="food"
-          onChange={e => onChange(e)}
-          className="form-control my-3"
+          placeholder="Ingredient name"
+          onChange={(e) => onChange(e)}
+          className="foodinput"
         />
         <input
           type="text"
           name="unit"
           value={unit}
           placeholder="unit"
-          onChange={e => onChange(e)}
+          onChange={(e) => onChange(e)}
           className="form-control my-3"
         />
         <input
@@ -53,7 +54,7 @@ setInputs({ ...inputs, [e.target.name]: e.target.value });
           name="carbon"
           value={carbon}
           placeholder="carbon"
-          onChange={e => onChange(e)}
+          onChange={(e) => onChange(e)}
           className="form-control my-3"
         />
         <button className="btn btn-success">Add</button>
@@ -64,6 +65,40 @@ setInputs({ ...inputs, [e.target.name]: e.target.value });
 
 export default InputFood;
 
+<div>
+  <form>
+    {formFields.map((form, index) => {
+      return (
+        <div key={index} className="addcontent">
+          {/*<SearchBar
+          placeholder="Ingredient name"
+          data={MOCKDATA}
+          onChange={(event) => handleFormChange(event, index)}
+          value={form.ingredient_name}
+        /> */}
+          <input
+            name="ingredient_name"
+            placeholder="Ingredient name"
+            onChange={(event) => handleFormChange(event, index)}
+            value={form.ingredient_name}
+            className="ingredientinput"
+          ></input>
+          <input
+            name="c_footprint"
+            placeholder="Carbon footprint/kg"
+            onChange={(event) => handleFormChange(event, index)}
+            value={form.c_footprint}
+            className="footprintinput"
+          ></input>
+          <button className="trashicon">
+            <FontAwesomeIcon icon={faTrash} />
+          </button>
+          <button className="submitbuttoning">Submit</button>
+        </div>
+      );
+    })}
+  </form>
+</div>;
 
 /*<input
           type="text"
