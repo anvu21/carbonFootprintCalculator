1b87bd92200d3b5db15b60f5e76bc9964bc6ab20
anvu21 <Vuphamducanh2000@gmail.com>
2022-06-13 00:38:24 -0400

    make it better

--- db.js
+++ db.js
@@ -9,6 +9,14 @@ require("dotenv").config();
 //   port: process.env.PG_PORT,
 // };
 
+const isProduction = process.env.NODE_ENV === "production";const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;const pool = new Pool({
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+/*
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig = process.env.DATABASE_URL; //heroku addons
@@ -18,5 +26,5 @@ const pool = new Pool({
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 //console.log(pool)
-
+*/
 module.exports = pool;
