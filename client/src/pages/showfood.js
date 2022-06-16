b378455ad0d8a7244fa97128e670c4f3d5101be0
anvu21 <Vuphamducanh2000@gmail.com>
2022-06-15 23:11:40 -0400

    e

---
+++ client/src/pages/showfood.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+//import ListFood from "./components/ListFood";
+import ListFood from "../components/ListFood";
+
+//client\src\components\ListFood.js
+const SignUp = () => {
+  return (
+    <Fragment>
+      <div className="container">
+        <ListFood />
+      </div>
+    </Fragment>
+  );
+};
+  
+export default SignUp
