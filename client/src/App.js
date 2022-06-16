d73f94b57561543c2d34427df4a8ddd1bed700a3
iahudson <iah224@lehigh.edu>
2022-06-15 22:22:24 -0400

    edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
 import React, { Fragment } from "react";
 import "./App.css";
-
 //components
-
+import HeaderFooter from "./components/HeaderFooter";
 import InputFood from "./components/InputFood";
 import ListFood from "./components/ListFood";
+import InputRecipe from "./components/InputRecipe";
+import ViewFood from "./components/ViewFood";
+import ViewRecipe from "./components/ViewRecipe";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
-    <Fragment>
-      <div className="container">
-        <HeaderFooter />
-        <InputFood />
-        <ListFood />
-      </div>
-    </Fragment>
+    // <Fragment>
+    //   <div className="container">
+    //     <HeaderFooter />
+    //     <InputFood />
+    //     <ListFood />
+    //   </div>
+    // </Fragment>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ViewRecipe />} />
+          <Route path="/addrecipes" element={<InputRecipe />} />
+          <Route path="/viewingredients" element={<ViewFood />} />
+          <Route path="/addingredients" element={<InputFood />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
-//<ListFood />
-export default App;
--- client/src/components/HeaderFooter.js
+++ client/src/components/HeaderFooter.js
@@ -1,4 +1,10 @@
-// import { Link } from "react-router-dom";
+import { BrowserRouter as Router, NavLink } from "react-router-dom";
+// const Link = require("react-router-dom").Link;
+// const BrowserRouter = require("react-router-dom").BrowserRouter;
+
+// const Route = require("react-router-dom").Route;
+
+// const Link = require("react-router-dom").Link;
 import "./HeaderFooter.css";
 
 function HeaderFooter() {
@@ -6,28 +12,39 @@ function HeaderFooter() {
     <div>
       <header className="header">
         <div className="name">FOOD CARBON FOOTPRINT CALCULATOR</div>
-        <div className="image"></div>
-        <nav>
-          <ul>
-            <li>
-              {/* <Link to="/">View Recipes</Link> */}
-              View Recipes
-            </li>
-            <li>
-              {/* <Link to="/addrecipes">Add Recipes</Link> */}Add Recipes
-            </li>
-            <li>
-              {/* <Link to="/viewingredients">View Ingredients</Link> */} View
-              Ingredients
-            </li>
-            <li>
-              {/* <Link to="/addingredients">Add Ingredients</Link> */}Add
-              Ingredients
-            </li>
-          </ul>
-        </nav>
+        <img src="../../public/logo192.png" className="image" />
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <NavLink exact to="/">
+                  View Recipes
+                </NavLink>
+                View Recipes
+              </li>
+              <li>
+                <NavLink exact to="/addrecipes">
+                  Add Recipes
+                </NavLink>
+                Add Recipes
+              </li>
+              <li>
+                <NavLink exact to="/viewingredients">
+                  View Ingredients
+                </NavLink>
+                View Ingredients
+              </li>
+              <li>
+                <NavLink exact to="/addingredients">
+                  Add Ingredients
+                </NavLink>
+                Add Ingredients
+              </li>
+            </ul>
+          </nav>
+        </Router>
       </header>
-      <div className="footer">
+      {/* <div className="footer">
         <div>
           <img
             src="https://collegeaim.org/wp-content/uploads/2021/08/Lehigh-University-logo.png"
@@ -40,7 +57,7 @@ function HeaderFooter() {
         >
           Campus Sustainable Impact Fellowship
         </a>
-      </div>
+      </div> */}
     </div>
   );
 }
