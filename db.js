c783eedb12018cb1566a26ce70114d94abf884b9
anvu21 <Vuphamducanh2000@gmail.com>
2022-06-22 01:03:21 -0400

    e

--- db.js
+++ db.js
@@ -9,12 +9,17 @@ require("dotenv").config();
 //   port: process.env.PG_PORT,
 // };
 
-const isProduction = process.env.NODE_ENV === "production";const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
+// const isProduction = process.env.NODE_ENV === "production";const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;const pool = new Pool({
+//     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//     ssl: {
+//         rejectUnauthorized: false,
+//     },
+// });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL || `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`,
+    ssl: process.env.DATABASE_URL ? true : false
+})
+
 
 /*
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT=process.env.PORT ||5000;
 //middleware
 app.use(cors());
 app.use(express.json())
-app.use(express.static("./client/build"))
+//app.use(express.static("./client/build"))
 
 if (process.env.NODE_ENV === 'production'){
   //server static conetent
